965d8b8b27cac90e6b489158def89fdd8204cbe2
Danimy <indaradani@gmail.com>
2017-01-10 09:08:59 +0700

    Create cedos.js

---
+++ cedos.js
@@ -0,0 +1,39 @@
+<script>
+  document.addEventListener('DOMContentLoaded', function() {
+   if(typeof getCookie('cinsys') == 'undefined'){
+   
+    setCookie('cinsys' , 'true' , 1);
+    setTimeout(function(){
+     document.getElementById("csen").style.opacity = "1";
+    } , 3000);  //waktu 3 menit tampil
+   }
+  });
+
+  function setCookie(cname, cvalue, exdays) {
+     var d = new Date();
+     d.setTime(d.getTime() + (exdays*24*60*60*1000));
+     var expires = "expires="+d.toUTCString();
+     document.cookie = cname + "=" + cvalue + "; " + expires;
+ }
+ function getCookie(name) {
+   var value = "; " + document.cookie;
+   var parts = value.split("; " + name + "=");
+   if (parts.length == 2) {
+       document.getElementById("csen").remove()
+     return parts.pop().split(";").shift();
+    }
+ }
+</script>
+
+<div id="csen">
+<style type='text/css'>
+#trblack{padding-left:150px;filter:alpha(opacity=60);opacity:.0;position:fixed;_position:absolute;top:20px;left:0;z-index:10;clip:inherit;_top:expression(document.documentElement.scrollTop+document.documentElement.clientHeight-this.clientHeight);_left:expression(document.documentElement.scrollLeft+document.documentElement.clientWidth - offsetWidth)}
+#samping {padding-left:280px;}
+</style>
+<div id='trblack'>
+<ins class='adsbygoogle' data-ad-client='ca-pub-4033768941373197'  data-ad-slot='9549102262' style='display:inline-block;width:300px;height:600px'></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+<ins class='adsbygoogle' data-ad-client='ca-pub-4033768941373197' data-ad-slot='9549102262' id='samping' style='display:inline-block;width:300px;height:600px'></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+</div>
+</div>
